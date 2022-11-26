cd86d68b425b6094353d773f57c7664158607288
Abdulrezak MOhamed <98439067+RezikEffort@users.noreply.github.com>
2022-11-25 19:01:01 -0500

    eslint fix

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -11,9 +11,9 @@
     https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 */
 function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+  const newmin = Math.ceil(min);
+  const newmax = Math.floor(max);
+  return Math.floor(Math.random() * (newmax - newmin + 1) + newmin); // The maximum is inclusive and the minimum is inclusive
 }
 
 function injectHTML(list) {
